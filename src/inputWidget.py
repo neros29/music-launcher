44f173e25050c7ab8c2438a559448378f1f209fa
neros29 <sorenmile29@gmail.com>
2026-09-10 23:46:00 -0600

    fix(inputWidget): fix bug were curser dosen't update when changed

--- src/inputWidget.py
+++ src/inputWidget.py
@@ -2,22 +2,20 @@ import string
 from typing import  List, Optional
 import time
 from pytui import  Surface
+from copy import copy
 from logging import getLogger
 
+logger = getLogger(__name__)
 class Token:
-    def __init__(self, fg, bg, ch, token_type="text", flash = None) -> None:
+    def __init__(self, fg, bg, ch) -> None:
         self.color = [fg, bg]
         self.character = ch if len(ch) == 1 else ch[0]
-        self.type = token_type
-        self.flash = flash
 
     def __eq__(self, value: object, /) -> bool:
         if isinstance(value, Token):
             colors = self.color == value.color
             characters = self.character == value.character
-            types = self.type == value.type
-            flashs = self.flash == value.flash
-            return colors and characters and types and flashs
+            return colors and characters
         return NotImplemented
     def __repr__(self):
         return self.character
@@ -38,11 +36,13 @@ class InputWidget:
             for x in range(self.width):
                 token = tokens[token_idx] if len(tokens) > token_idx else Token(self.fg, self.bg, " ")
                 if x == curser_pos[0] and y == curser_pos[1]:
+                    token = copy(token)
                     token.color = self.curser.color
                 token_idx += 1
                 idx = y * self.width + x
                 color = token.color
                 if self.current_surf[idx] != token:
+                    logger.debug("Updating coordinate, (%s, %s)", x, y)
                     self.surface[idx].set_fg(*color[0])
                     self.surface[idx].set_bg(*color[1])
                     self.surface[idx].set_ch(token.character)
